56fc3a69c0ec121e1f1f716b7757b4a854d1ac3b
Joao Paulo Magalhaes <biojppm>
2017-01-26 12:33:29 +0000

    fix find of nvtt

--- DevIL/src-IL/cmake/FindNVTT.cmake
+++ DevIL/src-IL/cmake/FindNVTT.cmake
@@ -1,104 +1,50 @@
 # Locate nvidia-texture-tools
 # This module defines
-# NVTT_LIBRARY
+# NVTT_LIBRARIES
 # NVTT_FOUND, if false, do not try to link to nvtt
 # NVTT_INCLUDE_DIR, where to find the headers
 #
 
 
-FIND_PATH(NVTT_INCLUDE_DIR nvtt/nvtt.h
-  PATHS
-  /usr/local
-  /usr
-  $ENV{NVTT_DIR}
-  ${3rdPartyRoot}
-  PATH_SUFFIXES include
-)
+#TARGET_LINK_LIBRARIES(nvtt PUBLIC ${LIBS} nvcore nvimage nvthread squish bc6h bc7 nvmath)
 
-# NVTT
-FIND_LIBRARY(NVTT_LIBRARY
-  NAMES nvtt
-  PATHS
-  /usr/local
-  /usr
-  $ENV{NVTT_DIR}
-  ${3rdPartyRoot}
-  PATH_SUFFIXES lib64 lib lib/shared lib/static lib64/static
-)
-
-FIND_LIBRARY(NVTT_LIBRARY_DEBUG
-  NAMES nvtt_d
-  PATHS
-  /usr/local
-  /usr
-  $ENV{NVTT_DIR}
-  ${3rdPartyRoot}
-  PATH_SUFFIXES lib64 lib lib/shared lib/static lib64/static
-)
-
-# NVIMAGE
-FIND_LIBRARY(NVIMAGE_LIBRARY
-  NAMES nvimage
-  PATHS
-  /usr/local
-  /usr
-  $ENV{NVTT_DIR}
-  ${3rdPartyRoot}
-  PATH_SUFFIXES lib64 lib lib/shared lib/static lib64/static
-)
-
-FIND_LIBRARY(NVIMAGE_LIBRARY_DEBUG
-  NAMES nvimage_d
-  PATHS
-  /usr/local
-  /usr
-  $ENV{NVTT_DIR}
-  ${3rdPartyRoot}
-  PATH_SUFFIXES lib64 lib lib/shared lib/static lib64/static
-)
-
-# NVMATH
-FIND_LIBRARY(NVMATH_LIBRARY
-  NAMES nvmath
-  PATHS
-  /usr/local
-  /usr
-  $ENV{NVTT_DIR}
-  ${3rdPartyRoot}
-  PATH_SUFFIXES lib64 lib lib/shared lib/static lib64/static
-)
-
-FIND_LIBRARY(NVMATH_LIBRARY_DEBUG
-  NAMES nvmath_d
-  PATHS
-  /usr/local
-  /usr
-  $ENV{NVTT_DIR}
-  ${3rdPartyRoot}
-  PATH_SUFFIXES lib64 lib lib/shared lib/static lib64/static
-)
-
-# NVCORE
-FIND_LIBRARY(NVCORE_LIBRARY
-  NAMES nvcore
-  PATHS
-  /usr/local
-  /usr
-  $ENV{NVTT_DIR}
-  ${3rdPartyRoot}
-  PATH_SUFFIXES lib64 lib lib/shared lib/static lib64/static
-)
-FIND_LIBRARY(NVCORE_LIBRARY_DEBUG
-  NAMES nvcore_d
-  PATHS
-  /usr/local
-  /usr
-  $ENV{NVTT_DIR}
-  ${3rdPartyRoot}
-  PATH_SUFFIXES lib64 lib lib/shared lib/static lib64/static
+set(_nvtt_search_paths
+    /usr/local
+    /usr
+    ${NVTT_DIR}
+    $ENV{NVTT_DIR}
+    ${3rdPartyRoot}
 )
 
 
+macro(_nvtt_search_lib var name)
+    find_library(${var}_LIBRARY NAMES ${name} ${_nvtt_search_paths}
+        PATH_SUFFIXES lib64 lib lib/shared lib/static lib64/static)
+    find_library(${var}_LIBRARY_DEBUG NAMES ${name}_d ${_nvtt_lib_paths})
+    if(${var}_LIBRARY_DEBUG AND ${var}_LIBRARY)
+        set(${var}_LINK_LIBRARY optimized ${${var}_LIBRARY} debug ${${var}_LIBRARY_DEBUG})
+    else()
+        set(${var}_LINK_LIBRARY ${${var}_LIBRARY})
+    endif()
+    if(${var}_LIBRARY OR ${var}_LIBRARY_DEBUG)
+        list(APPEND NVTT_LIBRARIES ${${var}_LINK_LIBRARY})
+    endif()
+endmacro()
+
+
+#----------------------------------------
+set(NVTT_LIBRARIES)
+
+FIND_PATH(NVTT_INCLUDE_DIR nvtt/nvtt.h ${_nvtt_search_paths})
+
+_nvtt_search_lib(NVTT nvtt)
+_nvtt_search_lib(NVTT_SQUISH squish)
+_nvtt_search_lib(NVTT_NVCORE nvcore)
+_nvtt_search_lib(NVTT_NVIMAGE nvimage)
+_nvtt_search_lib(NVTT_NVTHREAD nvthread)
+_nvtt_search_lib(NVTT_BC6H bc6h)
+_nvtt_search_lib(NVTT_BC7 bc7)
+_nvtt_search_lib(NVTT_MATH nvmath)
 
 
 SET(NVTT_FOUND "NO")
